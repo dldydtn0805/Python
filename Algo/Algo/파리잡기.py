96b6195dc38f689b166e7278f836f3e978120ebe
dldydtn0805 <ssafysoo95@gmail.com>
2023-08-03 00:21:39 +0900

    1

---
+++ Algo/Algo/파리잡기.py
@@ -0,0 +1,42 @@
+import sys
+sys.stdin = open('input.txt')
+
+def max_v(x,y):
+    if x > y:
+        return x
+    elif x < y:
+        return y
+    elif x == y:
+        return x
+
+T = int(input())
+for tc in range(1, T+1):
+    #N 크기의 전체 파리, M 크기의 파리채
+    N, M = map(int, input().split())
+    #전체 파리의 배열
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    #기본 파리채 설정
+    di = [0, 1, 1]
+    dj = [1, 1, 0]
+    max_total = 0
+    #파리 맵 순회
+    for i in range(N):
+        for j in range(N):
+            total = arr[i][j]
+            # 한번에 잡을 수 있는 총 파리 개수를 total 로 선언하고,
+            # 기준 인덱스의 파리 개수를 total 에 할당
+            for k in range(3):
+                # 기준 인덱스에서 파리채 모양으로 탐색
+                # 반복문 안에서 파리채 크기 M 만큼 곱해주면서 파리채 모양을 만든다
+                for p in range(M-1):
+                    idx_di = i+di[k]*p
+                    idx_dj = j+di[k]*p
+                    # total 에 해당 인덱스 더한다
+                    if 0 <= idx_di < N and 0 <= idx_dj < N:
+                        total += arr[idx_di][idx_dj]
+                    # 한번에 잡을 수 있는 총 파리의 개수의 최대값을 구한다
+
+                    print(total)
+            # max_total = max_v(total, max_total)
+    print(f'{tc}', max_total)
