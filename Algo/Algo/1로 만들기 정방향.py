c809020fae37749dc0c85d6cd8271add0249fa8e
dldydtn0805 <ssafysoo95@gmail.com>
2023-08-09 23:21:08 +0900

    1

---
+++ Algo/Algo/1로 만들기 정방향.py
@@ -0,0 +1,5 @@
+number = int(input())
+n = 1
+cnt = 0
+while True:
+    n = 3*n
--- Algo/Algo/1로 만들기.py
+++ Algo/Algo/1로 만들기.py
@@ -1,30 +1,20 @@
-"""
-https://www.acmicpc.net/problem/1463
-
-"""
 import sys
 sys.stdin = open('input.txt')
 
-for tc in range(2):
-    number = int(input())
-    x = 1
-    cnt = 0
-
-    while x < number:
-
-        x = x * 3
-        cnt += 1
-
-        if 3*x > number:
-            x= x* 2
-            cnt += 1
-
-        elif 2*x > number:
+number = int(input())
+x = 1
+cnt = 0
+# 숫자가 낮을때는 정상 작동하고
+# 숫자가 높을때는 정상 작동 안하는 이유는 뭘까???
+while x<number:
+    if 3*x > number:
+        if 2*x > number:
             x= x+1
             cnt += 1
-
-
-    print(cnt)
-
-
-
+        else:
+            x= x* 2
+            cnt += 1
+    else:
+        x = x*3
+        cnt += 1
+print(cnt)
