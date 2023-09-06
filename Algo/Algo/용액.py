7ee4e49e528007531baa80d1e7ee35731469f729
dldydtn0805 <ssafysoo95@gmail.com>
2023-09-06 22:22:14 +0900

    1

--- Algo/Algo/용액.py
+++ Algo/Algo/용액.py
@@ -2,6 +2,12 @@ import sys
 import heapq
 input = sys.stdin.readline
 
+
 n = int(input())
 water = list(map(int, input().split()))
-heap = [1e9]
+a, b = 0, 0
+min_v = 1e9
+
+def binary(start, end):
+    mid = (start + end-1) // 2
+    if min_v > water[mid]
