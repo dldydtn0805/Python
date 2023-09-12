14fbcd9f52e9afea3cbb6c269d6349a78f100402
dldydtn0805 <ssafysoo95@gmail.com>
2023-09-12 20:01:35 +0900

    1

--- Algo/Algo/나의 피파팀가치는.py
+++ Algo/Algo/나의 피파팀가치는.py
@@ -1,16 +1,21 @@
 import sys, heapq
 input = sys.stdin.readline
+# n은 선수의 수, k는 k년도 12월이 되었을때 팀의 선발 선수 가치의 합을 구해야하므로 선언
 n, k = map(int, input().split())
+# 11개 포지션의 선수들의 위치
 heap = [[] for _ in range(11)]
+# 포지션, 선수가치를 순차적으로 입력받아서 heap에 넣어준다 힙에 들어가는건 선수 가치 높은 순으로 정렬된다
 for _ in range(n):
     p, w = map(int, input().split())
     heapq.heappush(heap[p-1], -w)
+# k년 동안 11개 포지션을 순회하면서, 가장 가치가 높은 선수가 0보다 클때만 제일 비싼선수를 꺼내서 가치를 1 깎아준 다음 다시 넣어준다
 for _ in range(k):
     for i in range(11):
         if heap[i] and heap[i][0] + 1 < 0:
             x = heapq.heappop(heap[i])
             heapq.heappush(heap[i], x+1)
 result = 0
+# 최종 선수 가치를 result 값에 넣어준다
 for i in range(11):
     if heap[i]:
         x = -heap[i][0]
--- Algo/Algo/쿼드트리.py
+++ Algo/Algo/쿼드트리.py
@@ -5,40 +5,36 @@ input = sys.stdin.readline
 n = int(input())
 arr = [list(input()) for _ in range(n)]
 
-temp = ''
-result1 = ''
+temp = []
+result = []
 flag0 = 1
 flag1 = 1
 cnt = 0
-def quadtree(n,x,y):
+temp2 = []
+def quadtree(n,x,y,):
     global flag1, flag0, cnt
 
-
-    if n == 1:
-        if arr[x][y] == '1':
-            flag0 = 0
-        elif arr[x][y] == '0':
-            flag1 = 0
+    if n==1:
+        temp.append(arr[x][y])
         return
-
-
+    if len(temp) ==4:
+        # print(temp)
+        print('('*(n//2))
+        if len(set(temp)) == 1:
+            print(temp[0])
+            temp2.append(temp[0])
+        else:
+            print(*temp)
+        temp.clear()
+        print(')'*(n//2))
     quadtree(n//2, x, y)
     quadtree(n//2, x, y+n//2)
     quadtree(n//2, x+n//2, y)
     quadtree(n//2, x+n//2, y+n//2)
 
 
-    if flag0:
-        print(0)
-    elif flag1:
-        print(1)
-
-
-    flag0 = 1
-    flag1 = 1
-
 quadtree(n, 0, 0)
-
+print(result)
 '''
 n = int(input())
 board = [list(map(int, input())) for _ in range(n)]
