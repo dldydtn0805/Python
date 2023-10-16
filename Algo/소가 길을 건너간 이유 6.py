47ff8afc0b8d487e1369fa3077e9605b7b729a3e
dldydtn0805 <ssafysoo95@gmail.com>
2023-10-16 17:58:43 +0900

    1

--- Algo/소가 길을 건너간 이유 6.py
+++ Algo/소가 길을 건너간 이유 6.py
@@ -1,67 +1,50 @@
-"""
-목초지 정사각형 n x n
-
-인접한 목초지는 자유롭게 건널수 있다
-
-k마리의 소가 농장에 있다, 각 소는 서로 다른 목초지에 있다
-
-어떤 두 소는 길을 건너지 못하면 만나지 못한다
-
-(소의 위치를 기준으로 bfs를 돌리는데, 그 과정에서 길을 지났으면 카운트를 하나 더해준다)
-
-길을 지났는지 확인하는 법은 ?
-
-
-"""
 from collections import deque
-from itertools import combinations
 import sys
 sys. stdin = open('input.txt')
 
 def bfs(si, sj):
     visited[si][sj] = 1
     queue = deque()
-    queue.append((si,sj, str(si)+ ' ' +str(sj)))
+    queue.append((si,sj))
     while queue:
-        ci, cj, c_path = queue.popleft()
+        ci, cj = queue.popleft()
         for di, dj in [[1,0],[-1,0],[0,1],[0,-1]]:
             ni, nj = ci + di, cj + dj
+            if (ci, cj, ni, nj) in roads:
+                continue
             if 0<=ni<n and 0<=nj<n:
-                if not visited[ni][nj]:
-                    visited[ni][nj] = visited[ci][cj] + 1
-                    c_path = c_path + ' ' + str(ni) + ' ' + str(nj)
-                    queue.append((ni,nj, c_path))
+                if visited[ni][nj] == 0:
                     if ni == ei and nj == ej:
-                        return c_path
+                        return True
+                    visited[ni][nj] = visited[ci][cj] + 1
+                    queue.append((ni,nj))
+
 
 n, k, r = map(int, input().split())
 graph = [[0]*n for _ in range(n)]
-roads = []
+# 길..
+roads = set()
 cows = []
 
 for _ in range(r):
     r1, c1, r2, c2 = map(int, input().split())
-    temp1 = (str(r1 - 1) + ' ' + str(c1 - 1) + ' ' +  str(r2 - 1) + ' ' + str(c2 - 1))
-    temp2 = (str(r2 - 1) + ' ' + str(c2 - 1) + ' ' +  str(r1 - 1) + ' ' + str(c1 - 1))
-    roads.append(temp1)
-    roads.append(temp2)
+    # 양방향으로 길을 담아줬어
+    roads.add((r1 - 1, c1 - 1, r2 - 1, c2 - 1))
+    roads.add((r2 - 1, c2 - 1, r1 - 1, c1 - 1))
+
+# 소들을 담아줬어
 for _ in range(k):
     x, y = map(int, input().split())
-    graph[x-1][y-1] = 1
-    cows.append((x-1,y-1))
+    cows.append(((x-1),(y-1)))
 
 cnt = 0
-targets = list(combinations(cows, 2))
-for target in targets:
-    si, sj = target[0]
-    ei, ej = target[1]
+
+print(cows)
+# 2중포문 돌면서 bfs 해줬어
+for i in range(len(cows)):
+    si, sj = cows[i]
     visited = [[0]*n for _ in range(n)]
-    result = bfs(si, sj)
-    print(roads)
-    print(result)
-    for m in range(len(result)):
-        if m % 2 == 0:
-            if result[m:m+7] in roads:
-                cnt += 1
+    if not bfs(si,sj):
+        cnt += 1
 print(cnt)
 
--- Algo/소가 길을 건너간 이유.py
+++ Algo/소가 길을 건너간 이유.py
@@ -11,6 +11,9 @@ k마리의 소가 농장에 있다, 각 소는 서로 다른 목초지에 있다
 
 길을 지났는지 확인하는 법은 ?
 
+건너지 않으면 못만나는 경우 = 길을 막아놨는데도 만나는 경우를 세고, 그 나머지를 구하면 된다
+
+
 
 """
 from itertools import combinations
@@ -18,33 +21,31 @@ import sys
 sys.setrecursionlimit(10**6)
 sys. stdin = open('input.txt')
 
-def dfs(i, j, route):
+def dfs(i, j, temp):
+    global flag, route
     visited[i][j] = 1
     if i == ei and j == ej:
-        for road in roads:
-            print(road)
-            if road in route:
-                print(1)
+        print(temp)
         return
     for di, dj in [[1,0],[-1,0],[0,1],[0,-1]]:
         ni, nj = i + di, j + dj
         if 0<=ni<n and 0<=nj<n:
             if not visited[ni][nj]:
-                dfs(ni,nj, route+' '+str(ni)+str(nj))
+                for t in roads:
+                    t_r1, t_c1, t_r2, t_c2 = t
+                    if i == t_r1 and j == t_c1 and ni == t_r2 and nj == t_c2:
+                        flag = 1
+                dfs(ni,nj, temp + ' ' + str(ni) + ' ' + str(nj))
                 visited[ni][nj] = 0
-    return
-
 
 n, k, r = map(int, input().split())
 graph = [[0]*n for _ in range(n)]
 roads = []
 cows = []
-route = []
 for _ in range(r):
     r1, c1, r2, c2 = map(int, input().split())
-    roads.append([str(r1)+str(c1)+' '+str(r2)+str(c2)])
-    roads.append([str(r2) + str(c2) + ' ' + str(r1) + str(c1)])
-print(roads)
+    roads.append((r1-1,c1-1,r2-1,c2-1))
+
 for _ in range(k):
     x, y = map(int, input().split())
     graph[x-1][y-1] = 1
@@ -55,9 +56,10 @@ targets = list(combinations(cows, 2))
 for target in targets:
     si, sj = target[0]
     ei, ej = target[1]
+    flag = 0
+    route = set()
     visited = [[0]*n for _ in range(n)]
-    dfs(si, sj, str(si)+str(sj))
+    dfs(si, sj, str(si) + ' ' + str(sj))
+    print(flag)
 
-    print(visited)
-print(cnt)
 
