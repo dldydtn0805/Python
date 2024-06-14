211c04193edb2ba03a794ed82b42de519ce9b551
dldydtn0805 <139421083+dldydtn0805@users.noreply.github.com>
2024-06-14 18:20:28 +0900

    Create 11404.py

---
+++ Python/11404.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+import heapq
+
+def dijkstra(start_node):
+    heap = []
+    distanse = [1e9 for _ in range(n)]
+    heapq.heappush(heap, (0, start_node))
+    distanse[start_node] = 0
+    while heap:
+        current_weight, current_node = heapq.heappop(heap)
+        if current_weight > distanse[current_node]:
+            continue
+        for next_weight, next_node in adjacent_list[current_node]:
+            next_distance = next_weight + distanse[current_node]
+            if distanse[next_node] > next_distance:
+                distanse[next_node] = next_distance
+                heapq.heappush(heap, (next_distance, next_node))
+    return distanse
+
+n = int(input())
+m = int(input())
+adjacent_list = [[] for _ in range(n)]
+for _ in range(m):
+    a, b, c = map(int, input().rstrip().split())
+    a -= 1
+    b -= 1
+    adjacent_list[a].append([c, b])
+
+for i in range(n):
+    print(*dijkstra(i))
