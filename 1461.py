47c65ef2c5cb6f0af494c8da35a6fa2d82ea64df
dldydtn0805 <139421083+dldydtn0805@users.noreply.github.com>
2024-10-06 21:18:31 +0900

    Update 1461.py

--- 1461.py
+++ 1461.py
@@ -1,4 +1,12 @@
-import heapq
+def get_distance(X):
+    dp = [1e9 for _ in range(len(X))]
+    dp[0] = 2 * X[0]
+    for i in range(len(X)):
+        for j in range(M):
+            pi = i - j - 1
+            if 0 <= pi < len(X):
+                dp[i] = min((dp[pi] + 2 * X[i]), 2 * dp[i])
+    return dp
 N, M = map(int,input().split())
 books = list(map(int, input().split()))
 plus = []
@@ -13,24 +21,11 @@ plus.sort()
 minus.sort()
 minus = [0] + minus
 plus = [0] + plus
-minus_dp = [1e9 for _ in range(len(minus))]
-minus_dp[0] = 2*minus[0]
-for i in range(len(minus)):
-    for j in range(M):
-        pi = i - j -1
-        if 0<=pi<len(minus):
-            minus_dp[i] = min((minus_dp[pi] + 2 * minus[i]), 2*minus_dp[i])
 
-plus_dp = [1e9 for _ in range(len(plus))]
-plus_dp[0] = 2*plus[0]
-for i in range(len(plus)):
-    for j in range(M):
-        pi = i - j - 1
-        if 0<=pi<len(plus):
-            plus_dp[i] = min((plus_dp[pi] + 2 * plus[i]), 2*plus_dp[i])
-
+MINUS = get_distance(minus)
+PLUS = get_distance(plus)
 
 if minus[-1] > plus[-1]:
-    print(minus_dp[-1] + plus_dp[-1] - minus[-1])
+    print(MINUS[-1] + PLUS[-1] - minus[-1])
 else:
-    print(minus_dp[-1] + plus_dp[-1] - plus[-1])
+    print(MINUS[-1] + PLUS[-1] - plus[-1])
