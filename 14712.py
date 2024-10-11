e27031ac9fd80c69187f7fb5dda40c4fc077d126
dldydtn0805 <139421083+dldydtn0805@users.noreply.github.com>
2024-10-11 19:07:11 +0900

    Update 14712.py

--- 14712.py
+++ 14712.py
@@ -4,20 +4,20 @@ N, M = map(int, sys.stdin.readline().split())
 visited = [[0] * (M + 1) for _ in range(N + 1)]
 answer = 0
 
-def dfs(cnt):
+def dfs(depth):
     global answer
-    if cnt == N * M:
+    if depth == N * M:
         answer += 1
         return
 
-    y = cnt // M + 1
-    x = cnt % M + 1
+    ni = depth // M + 1
+    nj = depth % M + 1
 
-    dfs(cnt + 1)
-    if visited[y - 1][x] == 0 or visited[y][x - 1] == 0 or visited[y - 1][x - 1] == 0:
-        visited[y][x] = 1
-        dfs(cnt + 1)
-        visited[y][x] = 0
+    dfs(depth + 1)
+    if visited[ni - 1][nj] == 0 or visited[ni][nj - 1] == 0 or visited[ni - 1][nj - 1] == 0:
+        visited[ni][nj] = 1
+        dfs(depth + 1)
+        visited[ni][nj] = 0
 
 
 dfs(0)
