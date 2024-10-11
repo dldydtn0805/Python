dae56326b566b70f4236fec33d760074e092936f
dldydtn0805 <139421083+dldydtn0805@users.noreply.github.com>
2024-10-11 19:08:23 +0900

    Update 14712.py

--- 14712.py
+++ 14712.py
@@ -1,24 +1,19 @@
-import sys
-
-N, M = map(int, sys.stdin.readline().split())
-visited = [[0] * (M + 1) for _ in range(N + 1)]
-answer = 0
-
 def dfs(depth):
     global answer
     if depth == N * M:
         answer += 1
         return
-
     ni = depth // M + 1
     nj = depth % M + 1
-
     dfs(depth + 1)
     if visited[ni - 1][nj] == 0 or visited[ni][nj - 1] == 0 or visited[ni - 1][nj - 1] == 0:
         visited[ni][nj] = 1
         dfs(depth + 1)
         visited[ni][nj] = 0
 
+N, M = map(int, input().split())
+visited = [[0] * (M + 1) for _ in range(N + 1)]
+answer = 0
 
 dfs(0)
 print(answer)
